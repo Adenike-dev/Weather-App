0c97f6f519c21aca627c98a507f2353dd8b3509a
Adenike <akintoyeadenike@gmail.com>
2021-07-25 20:03:48 +0100

    Configured date and time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+function formatDate(datestamp) {
+  let date = new Date(datestamp);
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  let currentdate = date.getDate();
+  let months = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "October",
+    "November",
+    "December",
+  ];
+  let monthIndex = date.getMonth();
+  let month = months[monthIndex];
+  let year = date.getFullYear();
+  return `${day}, ${month}, ${currentdate} ${year}`;
+}
+function formatTime(timestamp) {
+  let currentTime = new Date(timestamp);
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+
+  return `${hours} : ${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#main-temp");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -14,6 +54,10 @@ function displayTemperature(response) {
   windElement.innerHTML = ` ${Math.round(response.data.wind.speed)}Km/H`;
   let feelElement = document.querySelector("#feels-like");
   feelElement.innerHTML = ` ${Math.round(response.data.main.feels_like)}°C`;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let timeElement = document.querySelector("#time-id");
+  timeElement.innerHTML = formatTime(response.data.dt * 1000);
 }
 
 let apiKey = "815a29c49f1689eb0317380664e5d969";
