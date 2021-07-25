9ac9e606b5fa3eb17f60ea7605d47fe2b7d27147
Adenike <akintoyeadenike@gmail.com>
2021-07-25 19:11:55 +0100

    Showing the current temperature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#main-temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let tempElement = document.querySelector("#temp-id");
+  tempElement.innerHTML = ` ${Math.round(response.data.main.temp)}°C`;
+  let humidElement = document.querySelector("#humid");
+  humidElement.innerHTML = ` ${Math.round(response.data.main.humidity)}%`;
+  let windElement = document.querySelector("#wind-id");
+  windElement.innerHTML = ` ${Math.round(response.data.wind.speed)}Km/H`;
+  let feelElement = document.querySelector("#feels-like");
+  feelElement.innerHTML = ` ${Math.round(response.data.main.feels_like)}°C`;
 }
 
 let apiKey = "815a29c49f1689eb0317380664e5d969";
